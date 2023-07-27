9b13b7872fb616265d978f3416c07df9affe2dc2
Mor Zahavi <mor@zahavi.xyz>
2023-07-27 13:03:21 +0300

    test

--- main.py
+++ main.py
@@ -908,23 +908,23 @@ def display_batch(batch, row=2, col=5):
     return
 
 
-ds = get_dataset(TRAIN_FILENAMES[:2], augment=False, cache=False, repeat=False).take(1)
-batch = next(iter(ds.unbatch().batch(20)))
-imgs, tars = batch
-# print(f'image_shape: {imgs.shape} target_shape:{tars.shape}')
-# print(f'image_dtype: {imgs.dtype} target_dtype:{tars.dtype}')
-display_batch(batch, row=3, col=3)
-plt.tight_layout()
-plt.savefig(f'{str(batch)}.png')
-plt.close()
-
-ds = get_dataset(TRAIN_FILENAMES[:2], augment=True, cache=False, repeat=False).take(1)
-batch = next(iter(ds.unbatch().batch(20)))
-imgs, tars = batch
-# print(f'image_shape: {imgs.shape} target_shape:{tars.shape}')
-# print(f'image_dtype: {imgs.dtype} target_dtype:{tars.dtype}')
-display_batch(batch, row=3, col=3)
-plt.tight_layout()
-plt.savefig(f'{str(batch)}.png')
-plt.close()
+# ds = get_dataset(TRAIN_FILENAMES[:2], augment=False, cache=False, repeat=False).take(1)
+# batch = next(iter(ds.unbatch().batch(20)))
+# imgs, tars = batch
+# # print(f'image_shape: {imgs.shape} target_shape:{tars.shape}')
+# # print(f'image_dtype: {imgs.dtype} target_dtype:{tars.dtype}')
+# display_batch(batch, row=3, col=3)
+# plt.tight_layout()
+# plt.savefig(f'{str(batch)}.png')
+# plt.close()
+#
+# ds = get_dataset(TRAIN_FILENAMES[:2], augment=True, cache=False, repeat=False).take(1)
+# batch = next(iter(ds.unbatch().batch(20)))
+# imgs, tars = batch
+# # print(f'image_shape: {imgs.shape} target_shape:{tars.shape}')
+# # print(f'image_dtype: {imgs.dtype} target_dtype:{tars.dtype}')
+# display_batch(batch, row=3, col=3)
+# plt.tight_layout()
+# plt.savefig(f'{str(batch)}.png')
+# plt.close()
 
