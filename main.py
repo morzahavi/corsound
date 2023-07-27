6f0d746a5c4c8fafb10a8bc594c94b8945f62e6e
Mor Zahavi <mor@zahavi.xyz>
2023-07-27 10:15:10 +0300

    test

--- main.py
+++ main.py
@@ -12,34 +12,34 @@ sns.set_style("whitegrid", {'axes.grid' : False})
 
 import tensorflow as tf
 
-# import tensorflow as tf, re, math
-# import tensorflow_addons as tfa
-# import tensorflow.keras.backend as K
-# import tensorflow_io as tfio
-# import tensorflow_probability as tfp
+import tensorflow as tf, re, math
+import tensorflow_addons as tfa
+import tensorflow.keras.backend as K
+import tensorflow_io as tfio
+import tensorflow_probability as tfp
 
-# import yaml
-# from IPython import display as ipd
-# import json
-# from datetime import datetime
-#
-# from glob import glob
-# from tqdm.notebook import tqdm
-# from kaggle_datasets import KaggleDatasets
-# import sklearn
-# from sklearn.metrics import confusion_matrix, precision_recall_fscore_support
-# from sklearn.model_selection import KFold
-# from sklearn.metrics import roc_auc_score
-# from IPython import display as ipd
-#
-# import itertools
-# import scipy
-# import warnings
-#
-# # Show less log messages
-# tf.get_logger().setLevel('ERROR')
-# tf.autograph.set_verbosity(0)
-#
-# # Set true to show less logging messages
-# os.environ["WANDB_SILENT"] = "true"
-# import wandb
+import yaml
+from IPython import display as ipd
+import json
+from datetime import datetime
+
+from glob import glob
+from tqdm.notebook import tqdm
+from kaggle_datasets import KaggleDatasets
+import sklearn
+from sklearn.metrics import confusion_matrix, precision_recall_fscore_support
+from sklearn.model_selection import KFold
+from sklearn.metrics import roc_auc_score
+from IPython import display as ipd
+
+import itertools
+import scipy
+import warnings
+
+# Show less log messages
+tf.get_logger().setLevel('ERROR')
+tf.autograph.set_verbosity(0)
+
+# Set true to show less logging messages
+os.environ["WANDB_SILENT"] = "true"
+import wandb
