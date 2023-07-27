69b6c2af018d2ffe72ab771471f16f14e9f56985
Mor Zahavi <mor@zahavi.xyz>
2023-07-27 10:16:21 +0300

    test

--- main.py
+++ main.py
@@ -42,3 +42,68 @@ tf.autograph.set_verbosity(0)
 # Set true to show less logging messages
 os.environ["WANDB_SILENT"] = "true"
 import wandb
+
+class CFG:
+    wandb = True
+    project = "fake-speech-detection"
+    debug = False
+    exp_name = "v0"
+    comment = "Conformer-128x80-cosine-no_aug-no_fc"
+
+    # Use verbose=0 for silent, 1 for interactive
+    verbose = 0
+    display_plot = True
+
+    # Device for training
+    device = None  # device is automatically selected
+
+    # Model & Backbone
+    model_name = "Conformer"
+
+    # Seeding for reproducibility
+    seed = 101
+
+    # Audio params
+    sample_rate = 16000
+    duration = 3.5 # duration in second
+    audio_len = int(sample_rate * duration)
+    normalize = True
+
+    # Spectrogram params
+    spec_freq = 128 # freq axis
+    n_fft = 2048
+    spec_time = 256 # time axis
+    hop_len = audio_len//(spec_time - 1) # non-overlap region
+    fmin = 20
+    fmax = sample_rate//2 # max frequency
+    spec_shape = [spec_time, spec_freq] # output spectrogram shape
+
+    # Audio Augmentation
+    timeshift_prob = 0.0
+    gn_prob = 0.0
+
+    # Spectrogram Augmentation
+    time_mask = 20
+    freq_mask = 10
+    cutmix_prob = 0.0
+    cutmix_alpha = 2.5
+    mixup_prob = 0.0
+    mixup_alpha = 2.5
+
+    # Batch Size & Epochs
+    batch_size = 32
+    drop_remainder = False
+    epochs = 12
+    steps_per_execution = None
+
+    # Loss & Optimizer & LR Scheduler
+    loss = "binary_crossentropy"
+    optimizer = "Adam"
+    lr = 1e-4
+    lr_schedule = "cosine"
+
+    # Augmentation
+    augment = False
+
+    # Clip values to [0, 1]
+    clip = False
