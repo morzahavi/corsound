61d32fca44af1253d4fd6ebc9492f35d1e661b81
Mor Zahavi <mor@zahavi.xyz>
2023-07-27 09:29:49 +0300

    test

--- cuda_install.sh
+++ cuda_install.sh
@@ -1,4 +1,4 @@
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.1-1_all.deb
-sudo dpkg -i cuda-keyring_1.1-1_all.deb
-sudo apt-get update
-sudo apt-get -y install cuda
+dpkg -i cuda-keyring_1.1-1_all.deb
+apt-get update
+apt-get -y install cuda
